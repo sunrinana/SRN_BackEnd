085c254d72036cc23a45fb866b04fea6a66ca841
Jaeho Cho <real0131@gmail.com>
2016-10-29 10:25:49 +0900

    finish

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,29 @@ var http = require('http');
 var url = require('url');
 var router = express.Router();
 var app = express();
+var Sender = require('node-xcs').Sender;
+var Result = require('node-xcs').Result;
+var Message = require('node-xcs').Message;
+var Notification = require('node-xcs').Notification;
 
-var FCM = require('fcm-push');
+var apiKey = 'AIzaSyD77o7ROigCo1lxzME8i92rR6IMGUvaR7U';
+var to = 'cud20fn1J2k:APA91bEMjTTvqcIN40pMpGRWXPtGcWV1NGV2Qbwd_0PjaCPUHN4nzivLS11ddipivsw4tPxdAomrlGkoknbAxZ2cmHJM3VpfwwqSf5uSbellcQf_BT4f5GZpMVBT3IOI09ioCqKMmbnW';
+var xcs = new Sender('66970132501', apiKey);
 
-var apiKey = 'AIzaSyDUDq4OA2GwkK8I2DTDa28kXBqGKeQ_cGI';
-var fcm = new FCM(apiKey);
+var notification = new Notification("ic_launcher")
+    .title("Hello buddy!")
+    .body("node-xcs is awesome.")
+    .build();
 
-var message = {
-  to : 'cud20fn1J2k:APA91bEMjTTvqcIN40pMpGRWXPtGcWV1NGV2Qbwd_0PjaCPUHN4nzivLS11ddipivsw4tPxdAomrlGkoknbAxZ2cmHJM3VpfwwqSf5uSbellcQf_BT4f5GZpMVBT3IOI09ioCqKMmbnW',
-  collapse_key : 'working!',
-  notification : {
-    title: 'Title of the notification',
-    body: 'Body of the notification'
-  }
-};
-
-
+var message = new Message("messageId_1046")
+    .priority("high")
+    .dryRun(false)
+    .addData("node-xcs", true)
+    .addData("anything_else", false)
+    .addData("awesomeness", 100)
+    .deliveryReceiptRequested(true)
+    .notification(notification)
+    .build();
 
 var client = new Twitter({
   consumer_key: 'Y5ylxpNdXqqSSdw03KACS6p7e',
@@ -32,11 +39,11 @@ var status;
 var isWorking = false;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fcm.send(message,function (err,res) {
-    if (err){
-      console.log("somthing has gone wrong");
-    }else{
-      console.log("successfully sent with res:",res);
+  xcs.sendNoRetry(message, to, function (result) {
+    if (result.getError()) {
+      console.error(result.getErrorDescription());
+    } else {
+      console.log("message sent: #" + result.getMessageId());
     }
   });
   res.render('index', { title: 'Express' });
